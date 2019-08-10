e8bf82b1194a49f9278ccee97a6a4cf7eff86fc8
Daniel Sadovskiy <danielsadovskiy@gmail.com>
2019-08-11 01:04:07 +0300

    Updates

---
+++ precache-manifest.6baa805fd78b8bfac4d1639fe212f6cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d16cf315bdfae4c7645466f5eee8941f",
+    "url": "/react-crud-shop/index.html"
+  },
+  {
+    "revision": "74c0f6782f38a477c591",
+    "url": "/react-crud-shop/static/css/2.d7a94725.chunk.css"
+  },
+  {
+    "revision": "ae76b348e9ed22cefaa2",
+    "url": "/react-crud-shop/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "74c0f6782f38a477c591",
+    "url": "/react-crud-shop/static/js/2.b312c818.chunk.js"
+  },
+  {
+    "revision": "ae76b348e9ed22cefaa2",
+    "url": "/react-crud-shop/static/js/main.a7d92459.chunk.js"
+  },
+  {
+    "revision": "3a7fb6c948fd563e7f7f",
+    "url": "/react-crud-shop/static/js/runtime~main.13bbc957.js"
+  }
+]);
