8b82361482135a288db543fc55affa542857f50e
Daniel Sadovskiy <danielsadovskiy@gmail.com>
2019-08-11 21:37:53 +0300

    Updates

--- precache-manifest.e1639ccf89c189cfcb3bc4f3294efa06.js
+++ precache-manifest.e1639ccf89c189cfcb3bc4f3294efa06.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5f6caa06ff30cfbe07ecf27a5635d80c",
+    "revision": "737c8c4a61ee99e9af4f7722b2f7955f",
     "url": "/react-crud-shop/index.html"
   },
   {
-    "revision": "830acc28350d9ae4532d",
+    "revision": "2236313014af8bf75e0f",
     "url": "/react-crud-shop/static/css/2.d7a94725.chunk.css"
   },
   {
-    "revision": "1118bace35b38e8e4518",
+    "revision": "ca95ac3439d47d56a01c",
     "url": "/react-crud-shop/static/css/main.cfda8877.chunk.css"
   },
   {
-    "revision": "830acc28350d9ae4532d",
-    "url": "/react-crud-shop/static/js/2.eb472dd6.chunk.js"
+    "revision": "2236313014af8bf75e0f",
+    "url": "/react-crud-shop/static/js/2.fe95e42c.chunk.js"
   },
   {
-    "revision": "1118bace35b38e8e4518",
-    "url": "/react-crud-shop/static/js/main.6e5de5f9.chunk.js"
+    "revision": "ca95ac3439d47d56a01c",
+    "url": "/react-crud-shop/static/js/main.3023e282.chunk.js"
   },
   {
     "revision": "3a7fb6c948fd563e7f7f",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-crud-shop/precache-manifest.db178d43861d1300bab8fd9532b66094.js"
+  "/react-crud-shop/precache-manifest.e1639ccf89c189cfcb3bc4f3294efa06.js"
 );
 
 self.addEventListener('message', (event) => {
