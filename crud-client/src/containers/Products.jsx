e58aa81f683bb4c5b5b70a9add2c5a4874345712
Daniel Sadovskiy <danielsadovskiy@gmail.com>
2019-08-11 18:08:21 +0300

    trying to fetch from github

--- crud-client/src/containers/Products.jsx
+++ crud-client/src/containers/Products.jsx
@@ -7,6 +7,14 @@ import React, { Component } from "react";
         isLoading: true,
         products: []
     };
+    componentDidMount(){
+        const API_PRODUCTS = 'https://github.com/DanielSadovskiy/react-crud-shop/blob/master/crud-client/src/products.json';
+        fetch(API_PRODUCTS)
+            .then(res => res.json())
+            .then(products =>{
+                console.log(products); 
+            })
+    }
     render() {
         return (
             <h1>Products</h1>
