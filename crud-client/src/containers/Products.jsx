fbac8b68b6a8b1c9f76e8504da83418b844878de
Daniel Sadovskiy <danielsadovskiy@gmail.com>
2019-08-11 19:07:46 +0300

    db is ok

--- crud-client/src/containers/Products.jsx
+++ crud-client/src/containers/Products.jsx
@@ -8,7 +8,7 @@ import React, { Component } from "react";
         products: []
     };
     componentDidMount(){
-        const API_PRODUCTS = 'https://randomuser.me/api/';
+        const API_PRODUCTS = 'https://github.com/DanielSadovskiy/react-crud-shop/blob/master/db.json';
         fetch(API_PRODUCTS)
             .then(res => res.json())
             .then(products =>{
